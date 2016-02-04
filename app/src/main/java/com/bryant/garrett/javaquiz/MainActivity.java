508e43a207eda8a019233e262fb47ad893dae320
Garrett Bryant <goofy70geb@gmail.com>
2016-02-04 15:44:16 -0500

    Created the button functionality. The True button creates a correct_response Toast The False button creates a wrong_response Toast

--- app/src/main/java/com/bryant/garrett/javaquiz/MainActivity.java
+++ app/src/main/java/com/bryant/garrett/javaquiz/MainActivity.java
@@ -13,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the question text
+        TextView mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        mQuestionTextView.setText(R.string.question);
+
+        // Set the true button response
+        Button mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.correct_response, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Set the false button response
+        Button mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.wrong_response, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
