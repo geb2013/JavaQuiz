9f3bd0f2df21cd9dc1fefa2ec074bab9ffabc6dd
Garrett Bryant <goofy70geb@gmail.com>
2016-02-18 14:03:44 -0500

    Created a new layout for landscape orientation

--- app/src/main/java/com/bryant/garrett/javaquiz/MainActivity.java
+++ app/src/main/java/com/bryant/garrett/javaquiz/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             questionBank = new QuestionBank();
            startingQuestion = questionBank.getNextQuestion();
         }
-        
+
         loadQuestion(startingQuestion);
         setNavigationListeners();
     }
